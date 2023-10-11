41956441ec2807237622031649c86b463d609070
Uygur Tepe <uygur.t10@gmail.com>
2023-10-11 11:26:03 -0400

    added resume link to resume page

--- src/routes/Resume/Resume.tsx
+++ src/routes/Resume/Resume.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
 import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
-import { styled } from '@mui/material/styles';
+import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import { styled } from "@mui/material/styles";
+import { Button, Typography } from "@mui/material";
 
 import experienceData from "../../constants/experience.json";
-import { Typography } from "@mui/material";
+
+// import TepeResume from "../../assets/TepeResume.pdf"; 
 
 interface Experience {
   id: number;
@@ -17,13 +20,12 @@ interface Experience {
 }
 
 const Item = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
+  textAlign: "center",
   color: theme.palette.text.secondary,
 }));
-
 
 const Resume = () => {
   const [experiences, setExperiences] = useState<Experience[]>([]);
@@ -41,14 +43,25 @@ const Resume = () => {
     <div>
       <Paper elevation={0}>
         <Typography variant="h3">Experiences</Typography>
+        {/* TODO: resolve this pdf import error */}
+        {/* <Button
+          variant="outlined"
+          color="primary"
+          endIcon={<PictureAsPdfIcon />}
+          href={TepeResume}
+          target="_blank"
+        >
+          View Full Resume
+        </Button> */}
         <Stack spacing={2}>
           {experiences.map((experience) => (
             <Item elevation={1}>
-              <Typography variant="h5">{experience.jobTitle}</Typography> 
-              <Typography variant="subtitle1">{experience.startDate} - {experience.endDate}</Typography>
+              <Typography variant="h5">{experience.jobTitle}</Typography>
+              <Typography variant="subtitle1">
+                {experience.startDate} - {experience.endDate}
+              </Typography>
               <Typography variant="h6">{experience.company}</Typography>
               <Typography variant="subtitle1">{experience.location}</Typography>
-              
             </Item>
           ))}
         </Stack>
