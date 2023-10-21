927c94a88091dfda15bba075b8408f4c7e73576e
Uygur Tepe <uygur.t10@gmail.com>
2023-10-21 16:13:06 -0400

    added experience component

---
+++ src/components/Experience/Experience.tsx
@@ -0,0 +1,69 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Typography from "@mui/material/Typography";
+import { styled } from "@mui/material/styles";
+import React from "react";
+import UnorderedList from "../UnorderedList/UnorderedList";
+
+export const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body1,
+  padding: theme.spacing(1),
+  color: theme.palette.text.secondary,
+}));
+
+export interface Experience {
+  id: number;
+  jobTitle: string;
+  company: string;
+  location: string;
+  startDate: string;
+  endDate: string;
+  description: string[];
+}
+
+type ExperienceCardProps = {
+    index: number;
+    experience: Experience;
+  };
+
+const ExperienceCard = ({ index, experience }: ExperienceCardProps) => {
+  return (
+    <Item key={index} elevation={1}>
+      <Grid
+        container
+        justifyContent="space-between"
+        alignItems="flex-start"
+        rowSpacing={1}
+        columnSpacing={{ xs: 1, sm: 2, md: 3 }}
+        sx={{ flexGrow: 1 }}
+      >
+        <Grid item xs={6}>
+          <Typography variant="h5">{experience.jobTitle}</Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="h6" sx={{ textAlign: "end" }}>
+            {experience.company}
+          </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography variant="subtitle1">
+            {experience.startDate} - {experience.endDate}
+          </Typography>
+        </Grid>
+        <Grid item xs={6} sx={{ textAlign: "end" }}>
+          <Typography variant="subtitle1">{experience.location}</Typography>
+        </Grid>
+      </Grid>
+      {/* TODO: ensure that links in description are clickable */}
+      <UnorderedList
+        elements={experience.description}
+        renderElement={(element) => (
+          <Typography variant="body1">{element}</Typography>
+        )}
+      />
+    </Item>
+  );
+};
+
+export default ExperienceCard;
