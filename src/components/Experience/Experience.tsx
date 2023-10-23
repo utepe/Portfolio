7dce1b61ebec3cfdbd0ac34a30f5b5bd54bdca81
Uygur Tepe <uygur.t10@gmail.com>
2023-10-23 15:46:46 -0400

    made experience text black

--- src/components/Experience/Experience.tsx
+++ src/components/Experience/Experience.tsx
@@ -9,7 +9,7 @@ export const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body1,
   padding: theme.spacing(1),
-  color: theme.palette.text.secondary,
+  color: theme.palette.text.primary,
 }));
 
 export interface Experience {
