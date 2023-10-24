a8b1cb9a8890fb8e4b7420e5cab486f6d90c24e6
Uygur Tepe <uygur.t10@gmail.com>
2023-10-24 16:12:20 -0400

    slightly modified home page

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -28,8 +28,8 @@ const Home = () => {
           <Typography variant="h5">A bit about me</Typography>
           <Typography variant="body1" sx={{ textAlign: "left" }}>
             I'm an engineer who loves to learn and build things. I'm currently heavily interested embedded systems and software development and bridging the gap between hardware and software. I have worked with a variety of tools and technologies and am always looking to learn more. My ultimate goal is to further improve the link between humans and computer systems to aid in the development of new technologies.
-            To do this I have worked in soft robotics, embedded systems and have aided in developing non-conventional Hardware-in-the-Loop system for thermal systems. I have recieved a B.A.S.c in Electrical and Computer Engineering from the University of Windsor.
-            I'm currently working as a Mechatronics engineer at {"Daimler Truck"} for the Powertrain Hardware-in-the-Loop Team. 
+            To do this I have worked in soft robotics, embedded systems and have aided in developing non-conventional Hardware-in-the-Loop environment for thermal systems. I have recieved a B.A.S.c in Electrical and Computer Engineering from the University of Windsor.
+            {/* I'm currently working as a Mechatronics Engineer at {"Daimler Truck"} for the Powertrain Hardware-in-the-Loop Team.  */}
           </Typography>
         </Grid>
       </Grid>
